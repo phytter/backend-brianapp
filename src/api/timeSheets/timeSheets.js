2f04e21932346032d518429543a9a452d4ab7e83
Alexandre <phytter@hotmail.com>
2020-12-06 00:37:25 -0300

    Versão minimalista

--- src/api/timeSheets/timeSheets.js
+++ src/api/timeSheets/timeSheets.js
@@ -5,6 +5,7 @@ const timeSheets = new mongoose.Schema({
     regarding:{type: String, require: true},
     // day_week:{type: String, require: true},
     days:{type: Array, require: false},
+    id_employer: {type: String, require: true},
 },
     {timestamps: {}}
 )
