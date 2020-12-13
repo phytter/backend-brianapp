3e92ccff4e0ba66982eb55caf07d1f8527c69160
Alexandre Silva <phytter@hotmail.com>
2020-12-13 12:14:27 -0300

    Troca de database

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-const banco = 'mongodb+srv://dbadmin:dbadmin@estacionamento-kvdqj.gcp.mongodb.net/fazendaSantaCruz?retryWrites=true&w=majority'
+const banco = 'mongodb+srv://api:K2LqgCTEog1usgaT@cluster0.j45ot.mongodb.net/painel?retryWrites=true&w=majority'
 module.exports = mongoose.connect(banco,{ useNewUrlParser: true, useUnifiedTopology: true })
 
 mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório." 
